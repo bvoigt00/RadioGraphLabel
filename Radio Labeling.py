2bbaea8a5f1b593e85540c536cca1cf7f8929d98
Gross <grossz9678@my.uwstout.edu>
2021-11-12 18:53:25 -0600

    Implementation of Floyd's

--- Radio Labeling.py
+++ Radio Labeling.py
@@ -3,16 +3,18 @@
 #############
 
 import copy
+import math
+import numpy
 
 ################
 ## Graph Init ##
 ################
 
 testGraph = [
-  [2],
+  [1],
+  [0, 2],
   [1, 3],
-  [2, 4],
-  [3]
+  [2]
 ]
 
 proposedLabeling = [0, 0, 0, 0]
@@ -26,26 +28,44 @@ def main():
     print("The labeling must contain an element for each vertex in the graph")
     return
   
-  print ("Hello World")
-  diameter = diameterOf(testGraph)
-  print("Diameter is : " + str(diameter))
+  distance = distanceMatrix(testGraph)
+  print("Distance Matrix")
+  printGraph(distance)
 
-  for v in testGraph:
+  diameter = numpy.amax(distance)
+  print("Diameter of testGraph")
+  print(int(diameter))
+
+# Floyd's Algorithm
+# Return a 2D array
+def distanceMatrix(graph):
+  # Initialize to infinity
+  distance = []
+  order = len(graph)
+  distance = numpy.full((order, order), numpy.inf)
+  # Set distance from vertex to itself as 0
+  for i in range(order):
+    for j in range(order):
+      if i == j:
+        distance[i][j] = 0
+  # Set distance for adjacent vertices to 1
+  for i in range(order):
+    for j in range(len(graph[i])):
+      distance[i][graph[i][j]] = 1
+  # Main loop
+  for k in range(order):
+    for i in range(order):
+      for j in range(order):
+        if distance[i][j] > distance[i][k] + distance[k][j]:
+          distance[i][j] = distance[i][k] + distance[k][j]
+
+  return distance
+
+def printGraph(graph):
+  for v in graph:
     for a in v:
-      print(a, end=" ")
+      print(int(a), end=" ")
     print()
-
-def diameterOf(graph):
-  temp = copy.deepcopy(graph)
-  
-  temp[1][0] = 0
-
-  for v in temp:
-    for a in v:
-      print(a, end=" ")
-    print()
-  # TODO: Calculate the diameter of testGraph
-  return -1
 
 #############
 ## Startup ##
